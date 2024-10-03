1cfdc87fe1ac22a2010b65dd709ba2776734da05
Maikel Salazar <maikelsalazar@gmail.com>
2024-10-03 14:40:56 -0600

    feat: Seven Segment Display library for Arduino

---
+++ src/SevenSegmentDisplay.cpp
@@ -0,0 +1,86 @@
+#include "sevenSegmentDisplay.h"
+
+SevenSegmentDisplay::SevenSegmentDisplay(bool commonPin, uint8_t pinA, uint8_t pinB, uint8_t pinC, uint8_t pinD, uint8_t pinE, uint8_t pinF, uint8_t pinG) : commonPin(commonPin)
+{
+    segmentPins[0] = pinA;
+    segmentPins[1] = pinB;
+    segmentPins[2] = pinC;
+    segmentPins[3] = pinD;
+    segmentPins[4] = pinE;
+    segmentPins[5] = pinF;
+    segmentPins[6] = pinG;
+    pinDpConnected = false;
+    init();
+}
+
+SevenSegmentDisplay::SevenSegmentDisplay(bool commonPin, uint8_t pinA, uint8_t pinB, uint8_t pinC, uint8_t pinD, uint8_t pinE, uint8_t pinF, uint8_t pinG, uint8_t pinDp) : SevenSegmentDisplay(commonPin, pinA, pinB, pinC, pinD, pinE, pinF, pinG)
+{
+    pinDpConnected = true;
+    this->pinDp = pinDp;
+    pinMode(pinDp, OUTPUT); // init() do not set the mode to the DP pin
+}
+
+void SevenSegmentDisplay::init()
+{
+    for (uint8_t segment = 0; segment < 7; segment++)
+    {
+        pinMode(segmentPins[segment], OUTPUT);
+    }
+}
+
+void SevenSegmentDisplay::display(uint8_t digit)
+{
+    if (digit < 0 || digit > 9)
+    {
+        return;
+    }
+
+    if (commonPin) // cathode?
+    {
+        for (uint8_t segment = 0; segment < 7; segment++)
+        {
+            digitalWrite(segmentPins[segment], cathodeDigitsMap[digit][segment]);
+        }
+    }
+    else // then it is anode
+    {
+        for (uint8_t segment = 0; segment < 7; segment++)
+        {
+            digitalWrite(segmentPins[segment], !cathodeDigitsMap[digit][segment]);
+        }
+    }
+}
+
+void SevenSegmentDisplay::off()
+{
+    uint8_t pinOff = commonPin ? 0x0 : 0x1;
+
+    for (uint8_t segment = 0; segment < 7; segment++)
+    {
+        digitalWrite(segmentPins[segment], pinOff);
+    }
+
+    if (pinDpConnected)
+    {
+        digitalWrite(pinDp, pinOff);
+    }
+}
+
+void SevenSegmentDisplay::dp(bool on)
+{
+    if (!pinDpConnected) // if not connected why do you call it?
+    {
+        return;
+    }
+
+    uint8_t pinOn = commonPin ? 0x1 : 0x0;
+
+    if (on)
+    {
+        digitalWrite(pinDp, pinOn);
+    }
+    else
+    {
+        digitalWrite(pinDp, !pinOn);
+    }
+}
---
+++ src/SevenSegmentDisplay.h
@@ -0,0 +1,101 @@
+#ifndef SevenSegmentDisplay_H
+#define SevenSegmentDisplay_H
+
+#include "Arduino.h"
+
+class SevenSegmentDisplay
+{
+public:
+    /**
+     * @param commonPin     Display type: Anode: false; Cathode: true
+     *
+     * @param pinA          Output Pin number connected to the A segment
+     *
+     * @param pinB          Output Pin number connected to the B segment
+     *
+     * @param pinC          Output Pin number connected to the C segment
+     *
+     * @param pinD          Output Pin number connected to the D segment
+     *
+     * @param pinE          Output Pin number connected to the E segment
+     *
+     * @param pinF          Output Pin number connected to the F segment
+     *
+     * @param pinG          Output Pin number connected to the G segment
+     */
+    SevenSegmentDisplay(bool commonPin, uint8_t pinA, uint8_t pinB, uint8_t pinC, uint8_t pinD, uint8_t pinE, uint8_t pinF, uint8_t pinG);
+
+    /**
+     *
+     * @param commonPin     Display type: Anode: false; Cathode: true
+     *
+     * @param pinA          Output Pin number connected to the A segment
+     *
+     * @param pinB          Output Pin number connected to the B segment
+     *
+     * @param pinC          Output Pin number connected to the C segment
+     *
+     * @param pinD          Output Pin number connected to the D segment
+     *
+     * @param pinE          Output Pin number connected to the E segment
+     *
+     * @param pinF          Output Pin number connected to the F segment
+     *
+     * @param pinG          Output Pin number connected to the G segment
+     *
+     * @param pinDp         Output Pin number connected to the Decimal Point(DP) segment
+     */
+    SevenSegmentDisplay(bool commonPin, uint8_t pinA, uint8_t pinB, uint8_t pinC, uint8_t pinD, uint8_t pinE, uint8_t pinF, uint8_t pinG, uint8_t pinDp);
+
+    /**
+     * @brief               Display a digit on the Seven Segment Display
+     *
+     * @param digit         digit to display
+     */
+    void display(uint8_t digit);
+
+    /**
+     * @brief               Turn on or turn off the Decimal Point (DP) segment
+     *
+     * @param on            true for turning on; false for turning off
+     */
+    void dp(bool on);
+
+    /**
+     * @brief               turn off all the segments, even the Decimal Point (DP) segment if set
+     */
+    void off();
+
+private:
+    bool commonPin;         /* false: Anode Common; true: Cathode Common */
+
+    uint8_t segmentPins[7]; /* storage for the the pines from A to G */
+
+    bool pinDpConnected;    /* if the DP segment pin is set or not. */
+
+    uint8_t pinDp;          /* Output Pin number connected to the Decimal Point segment */
+
+    /**
+     * The digits representation for a Cathode Seven Segment Display (commonPin is true)
+     * In the Anode Seven Segment Display representation is the inverse of this (commonPin is false)
+     */
+    bool cathodeDigitsMap[10][7] = {
+        {0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x0}, /* 0 */
+        {0x0, 0x1, 0x1, 0x0, 0x0, 0x0, 0x0}, /* 1 */
+        {0x1, 0x1, 0x0, 0x1, 0x1, 0x0, 0x1}, /* 2 */
+        {0x1, 0x1, 0x1, 0x1, 0x0, 0x0, 0x1}, /* 3 */
+        {0x0, 0x1, 0x1, 0x0, 0x0, 0x1, 0x1}, /* 4 */
+        {0x1, 0x0, 0x1, 0x1, 0x0, 0x1, 0x1}, /* 5 */
+        {0x1, 0x0, 0x1, 0x1, 0x1, 0x1, 0x1}, /* 6 */
+        {0x1, 0x1, 0x1, 0x0, 0x0, 0x0, 0x0}, /* 7 */
+        {0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1}, /* 8 */
+        {0x1, 0x1, 0x1, 0x0, 0x0, 0x1, 0x1}  /* 9 */
+    };
+
+    /**
+     * @brief               Set the pines (A-G) to mode OUTPUT.
+     */
+    void init();
+};
+
+#endif
